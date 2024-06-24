6be1d24fc3da50adf4e4ac6b2139eba0618b3b5e
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-06-24 19:08:34 -0300

    Merge fix

--- Hotel/src/ui/ViewWeb.java
+++ Hotel/src/ui/ViewWeb.java
@@ -11,44 +11,36 @@ import javax.swing.JPanel;
 import facade.FacadeHoteleria;
 
 public class ViewWeb extends JPanel {
-<<<<<<< HEAD
-    private JFrame frame;
-
-    public ViewWeb() {
-        this.add(new JLabel("View Web"));
-        this.setVisible(true);
-
-        frame = new JFrame("Sistema de Gestión Hotelera");
-        frame.setSize(400, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel();
-        frame.add(panel);
-
-        // CARGA HUESPED
-        JButton btnCargarHuesped = new JButton("Cargar Nuevo Huesped");
-        btnCargarHuesped.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mostrarFormularioCargaHuesped();
-            }
-        });
-        panel.add(btnCargarHuesped);
-
-        frame.setVisible(true);
-    }
-
-    // Método para mostrar el formulario de carga de huésped
-    private void mostrarFormularioCargaHuesped() {
-        // Aquí implementas la lógica para mostrar el formulario de carga de huésped
-    }
-=======
-  FacadeHoteleria facade;
+  private FacadeHoteleria facade;
+  private JFrame frame;
 
   public ViewWeb(FacadeHoteleria facade) {
     this.facade = facade;
     this.add(new JLabel("View Web"));
     this.setVisible(true);
+
+    frame = new JFrame("Sistema de Gestión Hotelera");
+    frame.setSize(400, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JPanel panel = new JPanel();
+    frame.add(panel);
+
+    // CARGA HUESPED
+    JButton btnCargarHuesped = new JButton("Cargar Nuevo Huesped");
+    btnCargarHuesped.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        mostrarFormularioCargaHuesped();
+      }
+    });
+    panel.add(btnCargarHuesped);
+
+    frame.setVisible(true);
   }
->>>>>>> 6ea34d8f993cdadafa930e912266c364d1f5c21b
+
+  // Método para mostrar el formulario de carga de huésped
+  private void mostrarFormularioCargaHuesped() {
+    // Aquí implementas la lógica para mostrar el formulario de carga de huésped
+  }
 }
